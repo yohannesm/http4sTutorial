6bc4584231e17a7414ea2d4e12ab097e9568d7c1
Marcell Himawan <marcell.himawan@hulu.com>
2024-03-06 15:07:59 -0800

    cleaned up files

--- src/main/scala/disneystreaming/http4stutorial/Main.scala
+++
@@ -1,8 +0,0 @@
-package disneystreaming.http4stutorial
-
-//import cats.effect.{ExitCode, IO, IOApp}
-//
-//object Main extends IOApp {
-//  def run(args: List[String]) =
-//    Http4stutorialServer.stream[IO].compile.drain.as(ExitCode.Success)
-//}
